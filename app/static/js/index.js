2ad2e5b8f2bc835f729f38f107e3598bd251f11f
sbluemin <yoonhyeonmin@gmail.com>
2018-02-03 19:34:21 +0900

    완성

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -32,6 +32,40 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+var sideBar = new Vue({
+    el: '#sidebar-wrapper',
+    methods: {
+        // 영혼 융합체: 영혼 대폭발
+        a: function(event) {
+            requestPostDamageTableAndUpdateDamageTable(createDamageJsonMessage("융합체", false));
+        },
+        // 혐오스러운 원한강타: 지축붕괴 발구르기
+        b: function(event) {
+            requestPostDamageTableAndUpdateDamageTable(createDamageJsonMessage("원한강타", false));
+        },
+        // 로크모라: 산산조각
+        c: function(event) {
+            requestPostDamageTableAndUpdateDamageTable(createDamageJsonMessage("로크모라", false));
+        },
+        // 자칼:사악한 격돌
+        d: function(event) {
+            requestPostDamageTableAndUpdateDamageTable(createDamageJsonMessage("자칼", false));
+        },
+        // 증오갈퀴 여군주: 집중된 번개
+        e: function(event) {
+            requestPostDamageTableAndUpdateDamageTable(createDamageJsonMessage("증오갈퀴여군주", false));
+        },
+        // 하임달: 뿔피리
+        f: function(event) {
+            requestPostDamageTableAndUpdateDamageTable(createDamageJsonMessage("하임달", false));
+        },
+        // 스코발드: 지옥화염쇄도
+        g: function(event) {
+            requestPostDamageTableAndUpdateDamageTable(createDamageJsonMessage("스코발드", false));
+        },
+    }
+});
+
 var damageTable = new Vue({
     el: '#damage_table',
     data: {
